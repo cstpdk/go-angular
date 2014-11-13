b9a12f092d08dc892aaf9fa03ed6f2049ca4c26b
Christoffer Pedersen <mail@cstp.dk>
2014-11-13 20:26:51 +0000

    Added rice, removed some sourcing

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-martini/martini"
+	"github.com/GeertJohan/go.rice"
 	"net/http"
 )
 
@@ -9,6 +10,7 @@ func main() {
 	m := martini.Classic()
 	m.Use(martini.Static("static"))
 
+	http.Handle("/", http.FileServer(rice.MustFindBox("static").HTTPBox()))
 	r := martini.NewRouter()
 	m.Action(r.Handle)
 
